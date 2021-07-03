057067c560864976a503e526320607a213576f36
MSang <msang77335@gmail.com>
2021-07-03 08:23:32 +0700

    create phuong-thuc-thanh-toan page

---
+++ pages/phuong-thuc-thanh-toan/index.tsx
@@ -0,0 +1,96 @@
+import React from "react";
+import Image from "next/image";
+import * as s from "../../styles/emotion/StylePayment";
+import { motion } from "framer-motion";
+
+const Payment: React.FC = () => {
+   return (
+      <motion.div
+         initial={{ opacity: 0 }}
+         animate={{ opacity: 1 }}
+         exit={{ opacity: 0 }}
+         transition={{ duration: 0.45 }}
+      >
+         <s.PaymentContentMain>
+            <s.PaymentContainer>
+               <s.Name>PHƯƠNG THỨC THANH TOÁN</s.Name>
+               <s.Top>
+                  <s.Des>
+                     Các bạn có thể lựa chọn 2 hình thức thanh toán sau khi mua
+                     sắm trực tuyến tại camelia.vn
+                  </s.Des>
+               </s.Top>
+               <s.Center>
+                  <s.Block>
+                     <s.Title>
+                        1. Thanh toán tiền mặt khi nhận hàng (COD)
+                     </s.Title>
+                     <s.Des>
+                        Bước 1: Các bạn mua hàng tại website camelia.vn và chọn
+                        'Thanh toán'
+                     </s.Des>
+                     <s.Des>
+                        Bước 2: Chọn ô 'Thanh toán tiền mặt khi nhận hàng' sau
+                        đó chọn 'Hoàn tất'
+                     </s.Des>
+                     <s.Image>
+                        <Image
+                           width="1055"
+                           height="846"
+                           layout="responsive"
+                           quality="100"
+                           src="https://file.hstatic.net/1000365849/file/phuongthucthanhtoantienmatcamelia_29a9d5846e314795b667f72251671104.jpg"
+                        />
+                     </s.Image>
+                     <s.Des>
+                        Đã hoàn tất! Nhân viên giao hàng sẽ nhận tiền mặt của
+                        bạn sau khi giao hàng.
+                     </s.Des>
+                     <s.Des>
+                        Chúng tôi sẽ liên lạc cho bạn trước khi sản phẩm đến
+                        nơi.
+                     </s.Des>
+                  </s.Block>
+                  <s.Block>
+                     <s.Title>2. Chuyển khoản ngân hàng</s.Title>
+                     <s.Des>
+                        Bước 1: Các bạn mua hàng tại website camelia.vn và chọn
+                        'Thanh toán'
+                     </s.Des>
+                     <s.Des>
+                        Bước 2: Chọn ô 'Chuyển khoản ngân hàng' và làm theo
+                        hướng dẫn cụ thể phía dưới
+                     </s.Des>
+                     <s.Image>
+                        <Image
+                           width="1047"
+                           height="1073"
+                           layout="responsive"
+                           quality="100"
+                           src="https://file.hstatic.net/1000365849/file/phuongthucthanhtoanonlinecamelia_6edcd0da46d843ac9b7968cb7fd362ca.jpg"
+                        />
+                     </s.Image>
+                     <s.Des>
+                        Đã hoàn tất! Camelia sẽ giao hàng cho bạn ngay khi chúng
+                        tôi vừa nhận được tiền.
+                     </s.Des>
+                     <s.Des>
+                        Chúng tôi sẽ liên lạc cho bạn trước khi sản phẩm đến
+                        nơi.
+                     </s.Des>
+                  </s.Block>
+               </s.Center>
+               <s.Bottom>
+                  <s.Des>
+                     Nếu quý khách có bất kỳ yêu cầu hay thắc mắc nào hoặc không
+                     hài lòng về sản phẩm/dịch vụ của Camelia, hãy liên hệ với
+                     chúng tôi tại thecameliavn@gmail.com hoặc 0909140170.
+                  </s.Des>
+               </s.Bottom>
+            </s.PaymentContainer>
+         </s.PaymentContentMain>
+      </motion.div>
+   );
+};
+
+export default Payment;
---
+++ styles/emotion/StylePayment.ts
@@ -0,0 +1,63 @@
+import styled from "@emotion/styled";
+import Container from "./StyleContainer";
+import ContentMain from "./StyleContentMain";
+
+export const PaymentContentMain = styled(ContentMain)``;
+
+export const PaymentContainer = styled(Container)`
+   padding-top: 2rem;
+`;
+
+export const Name = styled.h2`
+   text-transform: uppercase;
+   font-size: 2.3rem;
+   color: #333333;
+`;
+
+export const Des = styled.p`
+   color: #000000;
+   margin: 1.5rem 0;
+`;
+
+export const Title = styled.h4`
+   color: #000000;
+   font-size: 1.6rem;
+`;
+
+export const Image = styled.div`
+   max-width: 43rem;
+`;
+
+export const Block = styled.div`
+   margin: 4rem 0;
+   ${Title} {
+      font-size: 1.6rem;
+      text-transform: initial;
+   }
+`;
+
+export const Top = styled.div`
+   margin: 3rem 0;
+   ${Des} {
+      font-size: 1.6rem;
+   } ;
+`;
+
+export const Center = styled.div`
+   border-top: 0.5px solid #000;
+   border-bottom: 0.5px solid #000;
+   padding: 2rem 0;
+   ${Title} {
+      color: #000000;
+      font-size: 1.6rem;
+      font-weight: 700;
+   }
+`;
+
+export const Bottom = styled.div`
+   padding: 3rem 0 5rem;
+`;
+
+export const Bold = styled.div`
+   font-weight: 700;
+`;
