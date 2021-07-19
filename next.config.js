1a8cdd147f3ffdd2604ec7604ab1c61d263ddfb0
MSang <msang77335@gmail.com>
2021-07-19 18:32:04 +0700

    edit packageJson

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 const withPlugins = require("next-compose-plugins");
 const withImages = require("next-images");
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-   enabled: process.env.ANALYZE === "true",
-});
 
 const nextConfig = {
    images: {
@@ -14,4 +11,4 @@ const nextConfig = {
          "mongodb+srv://camelia:msang77335@cameliacluster.iyu2m.mongodb.net/camelia?retryWrites=true&w=majority",
    },
 };
-module.exports = withPlugins([[withImages], [withBundleAnalyzer]], nextConfig);
+module.exports = withPlugins([[withImages]], nextConfig);
